4b6fc6a6429996fce6b22c17bb7d2bfdb1b6fda4
Ling0 <1355733858@qq.com>
2024-03-27 23:00:14 +0800

    3/27/2024 7-52完成

---
+++ sse-driver-client-web/src/main/java/com/ling/ssedriverclientweb/SseDriverClientWeb.java
@@ -0,0 +1,12 @@
+package com.ling.ssedriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseDriverClientWeb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverClientWeb.class);
+    }
+}
