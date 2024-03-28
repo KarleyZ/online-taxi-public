5fd1b8ecefc9e9c4bb798aa9c7ada4a1ebcdcad1
Ling0 <1355733858@qq.com>
2024-03-28 20:17:02 +0800

    3/27/2024 7-56完成

--- service-sse-push/src/main/java/com/ling/servicessepush/SseDriverClientWeb.java
+++ service-sse-push/src/main/java/com/ling/servicessepush/SseDriverClientWeb.java
@@ -1,4 +1,4 @@
-package com.ling.ssedriverclientweb;
+package com.ling.servicessepush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-sse-push/src/main/java/com/ling/servicessepush/controller/SseController.java
+++ service-sse-push/src/main/java/com/ling/servicessepush/controller/SseController.java
@@ -1,4 +1,4 @@
-package com.ling.ssedriverclientweb.controller;
+package com.ling.servicessepush.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
